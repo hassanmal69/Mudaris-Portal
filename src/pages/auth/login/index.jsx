c220c2f60f0950e83ca6fc361b30eda0ebf8997a
hassanmal69 <158709739+hassanmal69@users.noreply.github.com>
2025-10-26 09:53:53 -0700

    loginDone

--- src/pages/auth/login/index.jsx
+++ src/pages/auth/login/index.jsx
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import { loginSchema } from "@/validation/authSchema";
-import { FarsiQuote } from "../../../constants/FarsiQuote";
 import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { loginUser } from "@/features/auth/authSlice";
@@ -41,18 +40,24 @@ const Login = () => {
   }, [navigate, from, session]);
 
   return pageloading ? (
-    <div className="min-h-screen flex relative flex-col md:flex-row bg-gradient-to-br from-[#220E3D] to-[#5B25A3]">
-      <div className="text-(--foreground) flex flex-col items-center w-[60%]">
+
+    <div className="font-poppins flex relative flex-col md:flex-row bg-[linear-gradient(20.82deg,#220E3D_11.13%,#5B25A3_88.87%)]">
+      <div className="text-white flex flex-col justify-center items-center w-[70%]">
         <div className="flex flex-col items-center text-left w-full h-full">
-          <h1
-            className=" font-extrabold text-[60px] leading-[70px]">
-            <span className="font-semibold text-[40px] leading-[70px]">Welcome to </span> <br />
-            Mudaris Academy <br /> Student Portal</h1>
-          <p className="font-poppins font-medium text-[16px] text-left">Login to access your account</p>
+          <div className="flex flex-col gap-2">
+            <h1
+              className=" font-extrabold text-[60px] leading-[70px]">
+              <span className="font-semibold text-[40px] leading-[70px]">Welcome to </span> <br />
+              Mudaris Academy <br /> Student Portal</h1>
+            <p className="font-poppins font-medium text-[16px]">Login to access your account</p>
+          </div>
         </div>
-        <img src={bgImg} alt="Mudaris Academy logo" srcset="" />
+        <img
+          className="w-[70%]"
+          src={bgImg}
+          alt="Mudaris Academy logo" />
       </div>
-      <div className="w-full  md:w-1/2 flex items-center justify-center bg-[#2A2C32] py-12 px-4">
+      <div className="w-full  md:w-1/2 flex items-center justify-center bg-[#2A2C32] text-white py-12">
         <Formik
           initialValues={{
             email: "",
@@ -66,65 +71,56 @@ const Login = () => {
         >
           {({ touched, errors, isSubmitting }) => (
             <Form
+              className="w-1/2 flex flex-col items-start gap-2"
               aria-label="Login form"
             >
-              <h2 className="text-2xl font-bold mb-8 text-center text-white dm-sans">
+              <h2 className="text-[48px] font-bold text-white dm-sans">
                 Sign in
               </h2>
+              <p className="opacity-70">Enter your account details</p>
+              <div className="w-full">
+                <div className="mb-5">
+                  <Field
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    className={`w-full py-2 focus:outline-none focus:ring-2 focus:ring-purple transition border-b-2 text-white placeholder-white/60 backdrop-blur-sm ${touched.email && errors.email
+                      ? "border-red-400"
+                      : "border-b-white/30"
+                      }`}
+                    placeholder="Email"
+                  />
+                  <ErrorMessage
+                    name="email"
+                    component="p"
+                    className="text-red-300 text-xs mt-1"
+                  />
+                </div>
 
-              <div className="mb-5">
-                <label
-                  htmlFor="email"
-                  className="block mb-2 text-white font-medium monts"
-                >
-                  Email
-                </label>
-                <Field
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  className={`w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-purple transition bg-white/20 text-white placeholder-white/60 backdrop-blur-sm ${touched.email && errors.email
-                    ? "border-red-400"
-                    : "border-white/30"
-                    }`}
-                  placeholder="Enter your email"
-                />
-                <ErrorMessage
-                  name="email"
-                  component="p"
-                  className="text-red-300 text-xs mt-1"
-                />
-              </div>
-
-              <div className="mb-6">
-                <label
-                  htmlFor="password"
-                  className="block mb-2 text-white font-medium monts"
-                >
-                  Password
-                </label>
-                <Field
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  className={`w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-purple transition bg-white/20 text-white placeholder-white/60 backdrop-blur-sm ${touched.password && errors.password
-                    ? "border-red-400"
-                    : "border-white/30"
-                    }`}
-                  placeholder="Enter your password"
-                />
-                <ErrorMessage
-                  name="password"
-                  component="p"
-                  className="text-red-300 text-xs mt-1"
-                />
+                <div className="mb-6">
+                  <Field
+                    id="password"
+                    name="password"
+                    type="password"
+                    autoComplete="current-password"
+                    className={`w-full py-2 border-b-2 focus:outline-none focus:ring-2 focus:ring-purple transition text-white placeholder-white/60 backdrop-blur-sm ${touched.password && errors.password
+                      ? "border-red-400"
+                      : "border-white/30"
+                      }`}
+                    placeholder="Password"
+                  />
+                  <ErrorMessage
+                    name="password"
+                    component="p"
+                    className="text-red-300 text-xs mt-1"
+                  />
+                </div>
               </div>
 
               <button
                 type="submit"
-                className="w-full bg-purple/80 backdrop-blur-sm text-white py-2 rounded-lg hover:bg-purple transition flex items-center justify-center font-semibold text-lg disabled:opacity-60 border border-white/20"
+                className="w-full font-poppins  bg-purple/80 bg-[#58259C] backdrop-blur-sm text-white py-2 rounded-lg hover:bg-purple transition flex items-center justify-center font-normal text-[16px] disabled:opacity-60 border border-white/20"
                 disabled={loading || isSubmitting}
                 aria-busy={loading}
               >
