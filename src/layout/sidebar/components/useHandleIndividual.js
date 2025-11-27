75cf76bb5254310c783dda06b728de86f9de3820
hassanmal69 <158709739+hassanmal69@users.noreply.github.com>
2025-11-28 00:35:57 +0500

    individualMsgEror

--- src/layout/sidebar/components/useHandleIndividual.js
+++ src/layout/sidebar/components/useHandleIndividual.js
@@ -8,8 +8,9 @@ const useHandleIndividual = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const handleFunction = (u) => {
-    console.log("us is", u);
-    const token = u?.id.slice(0, 6) + `${session?.user?.id.slice(0, 6)}`;
+    const ids = [u.id, session.user.id].sort()
+    console.log(ids)
+    const token = ids[0].slice(0, 6) + ids[1].slice(0, 6);
     navigate(`/workspace/${workspace_id}/individual/${token}`);
     dispatch(newDirect(u));
   };
